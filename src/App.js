949dcac323fd83c0bd4b79feb8adcc01197c8710
orangefruit21 <orangecat21@yandex.ru>
2020-10-13 01:12:17 +0300

    test components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = () => {
+    return (
+        <div>
+            <a href="/#">Home</a>
+            <a href="/#">Feed</a>
+            <a href="/#">Messages</a>
+        </div>
+    );
+}
+
+const Technologies = () => {
+    return (
+        <ul>
+            <li>HTML</li>
+            <li>JSX</li>
+            <li>JS</li>
+        </ul>
+    );
+}
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Technologies />
+        </div>
+    );
 }
 
 export default App;
