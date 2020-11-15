3ea42c2276d2aaf3aa732a313c16c63490a3e7d9
orangefruit21 <orangecat21@yandex.ru>
2020-11-16 00:03:03 +0300

    Create first own ref

--- src/components/Profile/Wall/NewPostForm/index.jsx
+++ src/components/Profile/Wall/NewPostForm/index.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import s from "./NewPostForm.module.css";
 
 const NewPostForm = () => {
+
+    const postElem = React.createRef();
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        let text = postElem.current.value;
+        alert(text);
+    }
+
     return (
-        <form action="#" className={s['post-form']}>
+        <form action="#" className={s['post-form']} onSubmit={event => submitHandler(event)}>
             <textarea name="postText"
                       placeholder="Your`s post..."
                       className={s['post-text']}
+                      ref={postElem}
             />
             <input type="submit"
                    value="Send"
