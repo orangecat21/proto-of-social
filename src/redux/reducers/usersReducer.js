30df15137716ff9ad5817ef967933d2c0ce41b08
orangefruit21 <orangecat21@yandex.ru>
2020-12-03 00:22:24 +0300

    get users list from API

--- src/redux/reducers/usersReducer.js
+++ src/redux/reducers/usersReducer.js
@@ -1,32 +1,7 @@
 import actionTypes from "../actionTypes";
 
 const initialState = {
-    users: [
-        {
-            id: 1,
-            followed: false,
-            photo: "",
-            userName: "Dmitry",
-            status: "I`m boss",
-            location: {city: "Minsk", country: "Belarus"}
-        },
-        {
-            id: 2,
-            followed: true,
-            photo: "",
-            userName: "Kate",
-            status: "I`m winner",
-            location: {city: "Moscow", country: "Russia"}
-        },
-        {
-            id: 3,
-            followed: false,
-            photo: "",
-            userName: "Elena",
-            status: "I`m healer",
-            location: {city: "Kiev", country: "Ukraine"}
-        },
-    ],
+    users: [],
 
 };
 
